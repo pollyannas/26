e7af5bd4026533d04d9b5e3921bd6f14a31f5f8b
Pollyanna <polle626@gmail.com>
2014-01-20 16:35:05 -0800

    gh-blog deployment: 2014-01-21T00:35:05.566173

--- everything.js
+++ everything.js
@@ -1,1 +1,7 @@
 var m;
+
+var blogimage = document.querySelector('.blog .image');
+if (blogimage) {
+  document.querySelector('.author .picture').src = blogimage.src
+  blogimage.style.display = 'none'
+}
